7405ab20354c4ee4199f3ffe8e74d8b37ebbd618
DonatoM <cruzmdonato@gmail.com>
2014-10-25 13:56:02 -0400

    Disregard the previous commit. The script prompts an error if no such files exists.

--- jct/execute.py
+++ jct/execute.py
@@ -5,7 +5,7 @@ import sys
 try:
 	if __name__ == "__main__":
 		files = " ".join(sys.argv[1:])
-		command = """ rm *.cpp *.hpp && mvn compile && clear && mvn exec:java -Dexec.mainClass="nyu.segfault.Translator" -Dexec.args="{0}" """.format(files)
+		command = """ mvn compile && clear && mvn exec:java -Dexec.mainClass="nyu.segfault.Translator" -Dexec.args="{0}" """.format(files)
 		subprocess.call(command,shell=True)
 except:
 		print("You did not enter a test file.")
