ec4087d93a64a79f4485a8d0b2a6922050d81f39
Jeff <jkl334@nyu.edu>
2014-10-25 18:43:35 -0400

    field declaration struct init progress

--- jct/src/main/java/nyu/segfault/SegFaultVisitor.java
+++ jct/src/main/java/nyu/segfault/SegFaultVisitor.java
@@ -207,6 +207,9 @@ public class SegFaultVisitor extends Visitor {
 	            } else if (declarationType.equals("String")) {
 	                impWriter.p("string ");
                 }
+                if (n.getNode(1).getNode(0).getName() == "QualifiedIdentifier") {
+                	impWriter.p(n.getNode(1).getNode(0).getString(0) + " ");
+                }
 
 				/* Print the name of the field. */
 				String fieldName = n.getNode(2).getNode(0).getString(0);
@@ -214,6 +217,11 @@ public class SegFaultVisitor extends Visitor {
 
 				/* Potentially visit the assigned value (if any). */
 				new Visitor() {
+
+					public void visitNewClassExpression(GNode n) {
+						impWriter.p(" = " + "(" + n.getNode(2).getString(0) + ")" + " {" + n.getNode(3).toString() + "}");
+					}
+					
 					public void visitStringLiteral(GNode n) {
 	                    impWriter.p(" = " + n.getString(0));
 	                }
