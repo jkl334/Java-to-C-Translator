420ac9402969a821bf29f7e85eb40cd6f39bab4b
Corey Masanto <coreymasanto@gmail.com>
2014-10-31 13:10:41 -0400

    Add helper class to facilitate GNode data access.

---
+++ jct/src/main/java/nyu/segfault/SegHelper.java
@@ -0,0 +1,24 @@
+package nyu.segfault;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.*;
+
+import javax.swing.tree.DefaultTreeModel;
+
+import xtc.lang.JavaFiveParser;
+import xtc.parser.ParseException;
+import xtc.parser.Result;
+import xtc.util.SymbolTable;
+import xtc.util.Tool;
+import xtc.tree.GNode;
+import xtc.tree.Node;
+import xtc.tree.Printer;
+import xtc.tree.Visitor;
+	
+public class SegHelper {
+			
+}
+
