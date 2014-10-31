bb2401653466a8e127f667ff070cc2d118b009f4
Alvin Mayers <alvin@alvincode.com>
2014-10-31 12:29:19 -0400

    init

---
+++ jct/src/main/java/nyu/segfault/SegHead.java
@@ -0,0 +1,39 @@
+package nyu.segfault;
+
+/**
+ * Java SE library import statements
+ */
+import java.io.*;
+import java.util.*;
+
+/**
+ * xtc import statements
+ */
+
+import xtc.lang.JavaFiveParser;
+
+import xtc.parser.ParseException;
+import xtc.parser.Result;
+
+import xtc.util.SymbolTable;
+import xtc.util.Tool;
+
+import xtc.tree.GNode;
+import xtc.tree.Node;
+import xtc.tree.Printer;
+import xtc.tree.Visitor;
+
+
+/**
+ * SegHead Visitor  handles classes without inheritance and virtual methods
+ */
+public class SegHead{
+
+	/**
+	 * constructor for SegHead
+	 */
+	public SegHead(){
+
+
+	}
+}
---
+++ jct/src/main/java/nyu/segfault/SegNode.java
@@ -0,0 +1,38 @@
+/**
+ * nonbinary unbalanced tree
+ * pre-order traversal
+ */
+public class SegNode<T>{
+	public T data;
+	public SegNode<T> parent;
+	public ArrayList<SegNode<T>> children;
+	/**
+	 * @param data create node with specified data
+	 */
+	public SegNode(T data){
+		this.data=data;
+		children=new ArrayList<SegNode<T>>();
+	}
+	/**
+	 * @param data add child node to Tree with specified data
+	 */
+	public void addChild(T data){
+		SegNode<T> child=new SegNode<T>(data);
+		child.parent=this;
+		this.children.add(child);
+	}
+	/**
+	 * @param n root node of subtree to traverse through
+	 * @param data data to find in tree
+	 */
+	public SegNode<T> dfs(SegNode<T> n, T data){
+		SegNode<T> found=n;
+		if(n.data.equals(data))  return n;
+		if(n.children.size() > 0)
+			for (SegNode<T> sn : n.children){
+				found=dfs(sn,data);
+				if(found != null) break;
+			}
+		return found;
+	}
+}
