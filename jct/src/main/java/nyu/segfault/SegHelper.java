22219cde57bc2b274a3b685fa3b7cd0411e3c913
Corey Masanto <coreymasanto@gmail.com>
2014-10-31 14:19:48 -0400

    Forced commit.

--- jct/src/main/java/nyu/segfault/SegHelper.java
+++ jct/src/main/java/nyu/segfault/SegHelper.java
@@ -18,7 +18,38 @@ import xtc.tree.Node;
 import xtc.tree.Printer;
 import xtc.tree.Visitor;
 	
-public class SegHelper {
-			
+public static class SegHelper {
+	/**
+	* Get the name of the method in visitMethodDeclaration.
+	*
+	* @param n	The node from the Java AST.
+	* @return	The name of the method.
+	*/
+	public static String getMethodName(GNode n) {
+		return n.getString(3);
+	}	
+
+	/**
+	* Get a list of visitCallExpression's parameters. 
+	* Must be used in visitExpressionStatement.
+	*
+	* @param n	The node from the Java AST.
+	* @return	An ArrayList<String> of parameters with the format ["Type name", "Type name", ...].
+	*/
+	public static ArrayList<String> getCallExpression(GNode n) {
+		ArrayList<String> parameters = new ArrayList<String>();
+		
+	
+	}	
+
+	/**
+	* 
+	*
+	*
+	*/
+	public static boolean isPrintStatement(GNode n) {
+		return (n.toString().contains("CallExpression") && n.toString().contains("SelectionExpression") && n.toString().contains("System") && n.toString().contains("out"));
+
+	}
 }
 
