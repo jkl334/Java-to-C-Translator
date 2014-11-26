b2905ebd4ac6123e1db1b55a0c2f18f3e3cf99df
DonatoM <cruzmdonato@gmail.com>
2014-11-26 13:49:56 -0500

    Fixed syntax error in SegHelper line 269

--- jct/src/main/java/nyu/segfault/SegImp.java
+++ jct/src/main/java/nyu/segfault/SegImp.java
@@ -25,23 +25,23 @@ public class SegImp extends Visitor{
 
     private String[] files; // args passed from the translator
     private String fileName; // name of the file to be translated
-    
+
     private int count;
-    
+
     String cc_name; /**@var current class name (this) */
     String className;
-    
+
     public SegImp() {  }
-    
+
     public void visitCompilationUnit(GNode n) {
         SegHelper.writeMacros();
         SegHelper.getCallExpression(n);
         visit(n);
         SegHelper.cpp_flush();
     }
-    
+
     public String constructorProp; //global variable to store constructor property in class declaration and to use to assign arguments in struct initialization in visitFieldDeclaration -Jeff
-    
+
     public void visitClassDeclaration(GNode n) {
         String className = SegHelper.getClassDeclaration(n).split(" ")[1];  // Set the SegHelper's curr_class, and returns the name of the current class.
 //        SegHelper.cpp_pln("class " + className + " {");
@@ -51,7 +51,7 @@ public class SegImp extends Visitor{
 //        SegHelper.cpp_pln("}\n");
 
     }
-    
+
     public void visitMethodDeclaration(GNode n){
         String declaration = SegHelper.getMethodDeclaration(n, "SegImp");
         declaration = declaration.substring(0, declaration.length() - 1);  // Remove the semi-colon.
@@ -62,7 +62,7 @@ public class SegImp extends Visitor{
         SegHelper.cpp_flush();
         System.out.println(SegHelper.getPointerFromMethodDeclaration(n));
     }
-    
+
     public void visit(Node n) { for (Object o : n) if (o instanceof Node) dispatch((Node)o); }
 
 }
